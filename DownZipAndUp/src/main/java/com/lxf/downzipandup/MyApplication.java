c302b3ecf1b115f48269f66dc6712a47b2a76f0e
lixiaofei17 <1195955974@qq.com>
2019-07-05 09:52:58 +0800

    文件下载并解压，retrofit网络请求 项目Module创建

---
+++ DownZipAndUp/src/main/java/com/lxf/downzipandup/MyApplication.java
@@ -0,0 +1,17 @@
+package com.lxf.downzipandup;
+
+import android.app.Application;
+
+/**
+ * @author: lixiaofei
+ * @date: 2019/7/3
+ * @version: 1.0.0
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
